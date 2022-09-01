c66b46e1e4185bda46868118ae46cc5997bd6d67
emman <41099196+emmanpsd@users.noreply.github.com>
2022-09-01 21:14:43 +0000

    Fixed bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,36 +11,37 @@ const Home: NextPage = () => {
         <meta name="description" content="built with nextjs + typescript" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div> 
+      <div>
         <div>
-          <img className={styles.logo}  src="https://i.ibb.co/WBXVr45/Untitled-2-Recovered.png" />
+          <img className={styles.logo} src="https://i.ibb.co/WBXVr45/Untitled-2-Recovered.png" />
         </div>
         <div>
         </div>
       </div>
 
+
       <main className={styles.main}>
 
         <div className={styles.intro}>
           <div>
-            <img src="https://cdn.discordapp.com/attachments/509806914050457610/1011407158241406996/IMG_0631.png" style={{height: '9em', width: '9em', borderRadius: 100,  }} />
+            <img src="https://cdn.discordapp.com/attachments/509806914050457610/1011407158241406996/IMG_0631.png" style={{ height: '9em', width: '9em', borderRadius: 100, }} />
           </div>
           <h2>Hi, I’m Emmanuel,</h2>
           <p>I’m a self taught fullstack web developer from Chicago who builds modern web/mobile applications with React.js and websites with WordPress.</p>
         </div>
-        
-        <button className={styles.button} style={{background: 'rgba(0, 114, 177, 0.85'}}><img src="" /><a href="https://www.linkedin.com/in/emmanuel-donelson/">LinkedIn Resume </a></button>
-         
-        <button className={styles.button} style={{background: 'rgba(234, 76, 137, 0.85)'}}><img src="" /><a href="https://dribbble.com/groovyemman" target="_blank">Dribbble Portfolio</a></button>
-        <button className={styles.button} style={{background: 'rgba(43, 49, 55, 0.85)'}}><img src="" /><a href="https://github.com/emmanpsd" target="_blank">Github</a></button>
-       
-        <button className={styles.button} style={{background: 'rgba(164,164,243, 0.85)'}}><img src="" /><a href="mailto:emmanueldonelson@gmail.com" target="_blank">Book Me</a></button>
-         <button className={styles.button} style={{background: 'rgba(240, 240, 0)', color:'black',fontWeight:'700'}}><img src="" /><a href="https://wh.npme2.repl.co/" target="_blank">Waffle House Project</a></button>
+
+        <button className={styles.button} style={{ background: 'rgba(0, 114, 177, 0.85' }}><a href="https://www.linkedin.com/in/emmanuel-donelson/">LinkedIn Resume </a></button>
+
+        <button className={styles.button} style={{ background: 'rgba(234, 76, 137, 0.85)' }}><a href="https://dribbble.com/groovyemman" target="_blank">Dribbble Portfolio</a></button>
+        <button className={styles.button} style={{ background: 'rgba(43, 49, 55, 0.85)' }}><a href="https://github.com/emmanpsd" target="_blank">Github</a></button>
+
+        <button className={styles.button} style={{ background: 'rgba(164,164,243, 0.85)' }}><a href="mailto:emmanueldonelson@gmail.com" target="_blank">Book Me</a></button>
+       <button className={styles.button} style={{ background: 'rgba(240, 240, 0)', color: 'black', fontWeight: '700' }}><a href="https://wh.npme2.repl.co/" target="_blank">Waffle House Project</a></button>
 
 
       </main>
 
-     
+
     </div>
   )
 }
